c5a160833e364fa68aa5794cfb335e395c1e508e
Philip <philip.h.hamilton@gmail.com>
2021-05-31 08:24:06 +0200

    Updated i2c communication between pi and arduino

--- Code/Python/Gui/Classes/RaspberryPi_I2C.py
+++ Code/Python/Gui/Classes/RaspberryPi_I2C.py
@@ -2,14 +2,38 @@ from smbus import SMBus
 import time
 
 SLAVE_ADRESS = 0x08
+SLAVE_ADRESS2 = 0x09
+HOME_MOTORS = 1
+MOVE_MOTORS = 2 
+
 
 class raspberryI2C():
-    def __init__(self, ):
+    def __init__(self):
         self.bus = SMBus(1)
+        self.pos = "0,0,0,0"
+        self.time = "0,0,0,0"
 
     def sendMessage(self, msg, slave_adress):
         msgInBytes = self.convertToBytes(msg)
-        self.bus.write_i2c_block_data(slave_adress, 0x00, msgInBytes)
+        self.bus.write_i2c_block_data(slave_adress, 0x30, msgInBytes)
+        
+    def updatePosition(self, x0, x1, x2, x3):
+        self.pos = "{},{},{},{}".format(x0,x1,x2,x3)
+
+    def updateTime(self, t0, t1, t2, t3):
+        self.time = "{},{},{},{}".format(t0,t1,t2,t3)
+
+    def homeMotors(self):
+        TASK = HOME_MOTORS
+        msg = "{},{},{}".format(TASK, self.pos, self.time)
+        self.sendMessage(msg, SLAVE_ADRESS)
+        self.sendMessage(msg, SLAVE_ADRESS2)
+
+    def moveMotors(self):
+        TASK = MOVE_MOTORS
+        msg = "{},{},{}".format(TASK, self.pos, self.time)
+        self.sendMessage(msg, SLAVE_ADRESS)
+        self.sendMessage(msg, SLAVE_ADRESS2)
 
     def readMessage(self):
         pass
@@ -20,20 +44,56 @@ class raspberryI2C():
         for byte in msgInBytes: 
             inBytes.append(byte)
         
-        print(inBytes)    
+        print(inBytes)
 
         return inBytes
 
 
 
+msg = "0,-5,-3,-2,4,-1,-1,-1,-1"
 a = raspberryI2C()
-msg = "0,1,2,3,4,5,6,7,8"
 
+b = "1,1,1,1"
+c = "2,2,2,2"
 
+d = "{},{}".format(b,c)
+
+print(d)
 
 while(1):
-    
-    a.sendMessage(msg, SLAVE_ADRESS)
+    x0 = input()
+    a.updatePosition(x0,0,0,0)
+    a.moveMotors()
+    # a.sendMessage(msg, SLAVE_ADRESS)
     time.sleep(1)
     
   
+# from smbus import SMBus
+# import time
+
+# SLAVE_ADRESS = 0x08
+
+# class raspberryI2C():
+#     def __init__(self, ):
+#         self.bus = SMBus(1)
+
+#     def sendMessage(self, msg, slave_adress):
+#         self.bus.write_i2c_block_data(SLAVE_ADRESS, 0, msg)
+#         # for byte in msg: 
+#         #     self.bus.write_byte(slave_adress, byte)
+
+#     def readMessage(self):
+#         pass
+
+
+# a = raspberryI2C()
+
+# # msg = [(startBit), (Position), (sign), ()]
+
+# array = [-1, 1, -1, 1]
+
+# while(1):
+#     a.sendMessage(array, SLAVE_ADRESS)
+#     print(array)
+#     time.sleep(5)
+    
--- Code/platformIO/src/Slave0.cpp
+++ Code/platformIO/src/Slave0.cpp
@@ -1,52 +1,246 @@
 #include <Arduino.h>
+#include "Class/axisClass.h"
 #include <Wire.h>
 
 #define SLAVE_ADRESS 0x08
 #define BAUD_RATE 9600
 
-byte recievedValues[9] = {};
-
-byte positionArray[6];
-byte timeArray[6];
-
-byte TASK = 0; 
+#define WAIT_FOR_INPUT 0
+#define HOME_MOTORS 1
+#define MOVE_MOTORS 2 
 
 
-void recieveEvent(int msgSize);
+// Motor parameters [Controller 0]
+// Axis 0
+uint8_t motorPins_0[3] = {5,4,6};
+uint16_t stepsPerRev_0 = 2*200;
+uint16_t frequencyRange_0[2] = {350, 2000};
+uint16_t angleRange_0[2] = {0, 1080};
+uint8_t limitSwitchPin_0 = 16;
+
+axisClass axis0(motorPins_0, stepsPerRev_0, frequencyRange_0, angleRange_0, limitSwitchPin_0);
+
+
+int testArray[9] = {};
+
+int positonArray[4] = {0, 0, 0, 0};
+int timeArray[4] = {-1, -1, -1, -1};
+
+int TASK = 0; 
+
+bool taskCompleted = true;
+
+void recieveEvent(int howMany);
+void requestEvent();
+void parseData(String array);
+void updateArray(int arr[]);
+void printArray();
+
+
+void homeMotors();
+void moveMotors(int positions[4], int times[4]);
+
 void setup()
 {
     Serial.begin(BAUD_RATE);
     Wire.begin(SLAVE_ADRESS);
     Wire.onReceive(recieveEvent);
+    Wire.onRequest(requestEvent);
 
 }
 
 void loop()
 {
 
+    switch(TASK)
+    {
+        case WAIT_FOR_INPUT:
+
+        break;
+
+        case HOME_MOTORS:
+            homeMotors();
+
+        break;
+
+        case MOVE_MOTORS:
+            moveMotors(positonArray, timeArray);
+        break;
+
+        default: 
+            TASK = 0; 
+        break;
+    }
+
+    if(taskCompleted)
+        TASK = 0; 
+
+}
+
+void requestEvent()
+{
+    // Get values
+    for(int i = 0; i < 3; i++)
+    {
+        Wire.write(i + '0');
+    }
 }
 
 void recieveEvent(int howMany)
 {
-    int counter = 0; 
+    char c;
+    String dataString = "";
+
     for(int i = 0; i < howMany; i++)
     {
-        if(i == 1)
-            TASK = Wire.read();
-        else if(i > 2 && i <=7)
-
-        else if(i > 8 && i <= 17)    
+        c = Wire.read();
+        dataString = dataString + c; 
     }
-    //     if( i%2 == 0)
-    //     {
-    //         recievedValues[counter] = Wire.read();
-    //         counter++;
-    //     }
-    // }
-    // for(int i = 0; i < counter; i++)
-    // {
-    //     Serial.print(recievedValues[i]);
-    // }
-    // Serial.println(" ");
+
+    parseData(dataString);
+    printArray();
+    dataString = "";
 }
 
+void updateArray(int arr[])
+{
+    TASK = arr[0]; 
+    for(int i = 0; i < 4; i++)
+    {
+        positonArray[i] = arr[1+i];
+        timeArray[i] = arr[5+i];
+    }
+}
+
+void parseData(String array)
+{
+    
+    int negativeNumber = 1; 
+    uint8_t counter = 0; 
+
+    // Starts at 1 to ignore the first 0
+    // Convert to array of ints
+    for(int i = 1; array[i] != '\0'; i++)
+    {
+        
+        if(array[i] == '-')
+        {
+            negativeNumber = -1; 
+            continue;
+        }
+        else if(array[i] == ',')
+        {
+            continue;
+        }
+        testArray[counter] = negativeNumber*(array[i] - '0');
+        negativeNumber = 1; 
+
+        counter++; 
+    }
+
+    updateArray(testArray);
+}
+
+void printArray()
+{
+    Serial.print("Task:");
+    Serial.print("\t");
+    Serial.println(TASK);
+
+    Serial.print("Position:");
+    Serial.print("\t");
+    Serial.print(positonArray[0]);
+    Serial.print(",");
+    Serial.print(positonArray[1]);
+    Serial.print(",");
+    Serial.print(positonArray[2]);
+    Serial.print(",");
+    Serial.println(positonArray[3]);
+    
+    Serial.print("Time:");
+    Serial.print("\t");
+    Serial.print(timeArray[0]);
+    Serial.print(",");
+    Serial.print(timeArray[1]);
+    Serial.print(",");
+    Serial.print(timeArray[2]);
+    Serial.print(",");
+    Serial.println(timeArray[3]);
+
+}
+
+void homeMotors(void)
+{
+  static int state = 0; 
+
+  switch(state)
+  {
+    case 0: 
+      axis0.home();
+      if(axis0.isHomed())
+      {
+        state++;
+      }
+    break; 
+
+    case 1: 
+      axis0.rotate(axis0.limitSwitchOffset);
+      if(axis0.ready())
+      {
+        taskCompleted = true; 
+        state = 0; 
+        Serial.println("Motors Homed");
+      }
+    break;
+  //   case 1: 
+  //     axis1.home();
+  //     if(axis1.isHomed())
+  //     {
+  //       axis1.rotate(axis1.limitSwitchOffset);
+  //       if(axis1.ready())
+  //         state++;
+  //     }
+  //   break;
+
+  //   case 2: 
+  //     axis2.home();
+  //     if(axis2.isHomed())
+  //     {
+  //       axis2.rotate(axis2.limitSwitchOffset);
+  //       if(axis2.ready())
+  //         state++;
+  //     }
+  //   break;
+
+  // case 3: 
+  //     axis3.home();
+  //     if(axis3.isHomed())
+  //     {
+  //       axis3.rotate(axis3.limitSwitchOffset);
+  //       if(axis3.ready())
+  //       {
+  //         taskCompleted = true; 
+  //         state = 0;
+  //       }
+  //     } 
+  //   break;
+
+    default: 
+      state = 0;
+    break; 
+  }
+}
+
+void moveMotors(int positions[4], int times[4])
+{
+  axis0.rotate((float)positions[0]);
+  if(axis0.ready())
+    taskCompleted = true;
+//   axis1.rotate(positions[1], times[1])
+//   axis2.rotate(positions[2], times[2])
+//   axis3.rotate(positions[3], times[3])
+  
+//   if(axis0.ready() && axis1.ready() && axis2.ready() && axis3.ready())
+//     taskCompleted = true; 
+// }
+}
